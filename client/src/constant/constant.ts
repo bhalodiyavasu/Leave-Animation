eaae4ea31266f2f803ed571fdc0e7f8461ecbc7c
bhalodiyavasu <vasubhalodiya49@gmail.com>
2026-07-14 17:07:44 +0530

    update

--- client/src/constant/constant.ts
+++ client/src/constant/constant.ts
@@ -1,1 +1,1 @@
-export const LEAVE_API_HOST = "https://leave-management-backend.vasu-prolix.workers.dev/api";
+export const LEAVE_API_HOST = "https://leave-management-backend.vasu-prolix.workers.dev";
