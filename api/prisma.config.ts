08e804f75669ab61896a2f6fef04be05e4410af0
yash gojariya <yash.prolix21@gmail.com>
2026-07-14 18:27:21 +0530

    chore: remove prisma migration commands from build script and clear default database url

--- api/prisma.config.ts
+++ api/prisma.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: process.env.DATABASE_URL || "mongodb+srv://vasu_admin:eternixAdminPass@eternixone.q1jk5mt.mongodb.net/prod_leaves?appName=eternixOne",
+    url: process.env.DATABASE_URL || "",
   },
 });
