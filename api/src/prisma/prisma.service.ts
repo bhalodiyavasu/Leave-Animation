521f3872e911c94a1db82c0622914904ff8a08b9
yash gojariya <yash.prolix21@gmail.com>
2026-07-14 18:44:53 +0530

    refactor: update PrismaClient import to edge runtime and standardize quote formatting

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate';
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-import { getEnvVar } from '../leave/worker-env.registry';
+import { getEnvVar } from "../leave/worker-env.registry";
 
 @Injectable()
 export class PrismaService implements OnModuleInit, OnModuleDestroy {
@@ -11,7 +11,7 @@ export class PrismaService implements OnModuleInit, OnModuleDestroy {
   get client() {
     if (!this._client) {
       this._client = new PrismaClient({
-        datasourceUrl: getEnvVar('DATABASE_URL'),
+        datasourceUrl: getEnvVar("DATABASE_URL"),
       }).$extends(withAccelerate());
     }
     return this._client;
